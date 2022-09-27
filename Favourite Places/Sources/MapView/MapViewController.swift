24038d730e55d1f7b311667a97460ac98a3b7a6f
golikovartyom <golikovartem404@gmail.com>
2022-09-27 10:52:40 +0400

    Add user location

--- Favourite Places/Sources/MapView/MapViewController.swift
+++ Favourite Places/Sources/MapView/MapViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
+    let regionPerimeter = 10000.0
 
     private lazy var mapView: MKMapView = {
         let map = MKMapView()
@@ -19,20 +22,36 @@ class MapViewController: UIViewController {
         return map
     }()
 
+    private lazy var userLocationButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "userlocation"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(centerViewInUserLocation), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupHierarchy()
         setupLayout()
         setupPlaceMark()
+        checkLocationServices()
     }
 
     private func setupHierarchy() {
         view.addSubview(mapView)
+        view.addSubview(userLocationButton)
     }
 
     private func setupLayout() {
         mapView.snp.makeConstraints { make in
             make.top.left.right.bottom.equalTo(view)
+        }
+
+        userLocationButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.snp.bottom).offset(-45)
+            make.right.equalTo(view.snp.right).offset(-30)
+            make.width.height.equalTo(50)
         }
     }
 
@@ -62,6 +81,60 @@ class MapViewController: UIViewController {
         }
     }
 
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let actionOK = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(actionOK)
+        present(alert, animated: true)
+    }
+
+    private func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Location services are disable",
+                               message: "You need to go to Settings and turn On")
+            }
+        }
+    }
+
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+
+    private func checkLocationAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case .denied:
+            // Show alert
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            // Show alert
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print("New case is available")
+        }
+    }
+
+    @objc func centerViewInUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionPerimeter,
+                                            longitudinalMeters: regionPerimeter)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+
 }
 
 extension MapViewController: MKMapViewDelegate {
@@ -81,5 +154,11 @@ extension MapViewController: MKMapViewDelegate {
             annotationView?.rightCalloutAccessoryView = imageView
         }
         return annotationView
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
     }
 }
